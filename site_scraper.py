7cddc5cf519c82905ee9bb3d58f46fb2a93dbfe6
Erik Albecker <erik.albecker@sap.com>
2025-12-01 21:14:02 +0100

    implement extract weekly information into python dict

--- site_scraper.py
+++ site_scraper.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup
 from requests import get
 from dotenv import load_dotenv
 import datetime
+from pprint import pprint
 
 def get_kw(date: datetime.date) -> int:
     calender = date.isocalendar()
@@ -21,11 +22,29 @@ def get_week_source(date: datetime.date = None) -> BeautifulSoup | None:
 
 
 def get_week_data(soup: BeautifulSoup) -> dict:
-    return {}
+    data = {}
+    div = soup.find_all("div", {"class": "canteen-day"})
+    for i, day in enumerate(div, start=1):
+        day_data = {}
+        for j, food_choice in enumerate(day.find_all("tr", {"class": "mensatype_rows"}), start=1):
+            food_choice_data = []
+            for option in food_choice.find_all("tr"):
+                if option.get("class") is None:
+                    continue
+                description = option.find("b").contents[0]
+                price = option.find("span", {"class": "bgp price_1"}).contents
+                if not price:
+                    continue
+                else:
+                    price = price[0]
+                food_choice_data.append((description, price))
+            day_data[f"Option {j}"] = food_choice_data
+        data[i] = day_data
+    return data
 
 def main():
     soup = get_week_source(datetime.date.today() + datetime.timedelta(weeks=0))
-    print(get_week_data(soup))
+    pprint(get_week_data(soup))
 
 
 if __name__ == '__main__':
